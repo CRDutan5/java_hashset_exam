7cd838bbd19ca9240110091c9113328be995c40e
Carlitos Dutan <carlitosdutan@pursuit.org>
2025-04-15 16:52:03 -0400

    completed exam

--- src/main/java/org/example/UserSet.java
+++ src/main/java/org/example/UserSet.java
@@ -1,7 +1,81 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class UserSet {}
+public class UserSet {
+
+    private Set<String> users = new HashSet<>();
+    private int userCount = 0;
+
+    public boolean addUser(String user) {
+        if (user == null || user.isEmpty() || user.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (containsUser(user)) {
+            return false;
+        }
+        else {
+            users.add(user);
+            userCount++;
+            return true;
+        }
+    }
+
+    public boolean containsUser(String user) {
+        return users.contains(user);
+    }
+
+    public int getUserCount() {
+        return userCount;
+    }
+
+    public boolean removeUser(String user) {
+        if (containsUser(user)) {
+            users.remove(user);
+            userCount--;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public HashSet<String> getUsersWithPrefix(String prefix) {
+        HashSet<String> filteredSet = new HashSet<>();
+        if (prefix == null) {
+            throw new IllegalArgumentException();
+        }
+        if (users.isEmpty()) {
+            return filteredSet;
+        }
+
+        for (String user : users) {
+            if (user.startsWith(prefix)) {
+                filteredSet.add(user);
+            }
+        }
+        return filteredSet;
+    }
+
+    public boolean areAllUsersUnique(String[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException();
+        }
+        if (arr.length == 0) {
+            return true;
+        }
+        HashSet<String> seenUsers = new HashSet<>();
+
+        for (String user : arr) {
+            if (seenUsers.contains(user)) {
+                return false;
+            }
+            else {
+                seenUsers.add(user);
+            }
+        }
+        return true;
+
+    }
+
+}
